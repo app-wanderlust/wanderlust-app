fa0e32e07b09b7108535bd825d17803261119b9a
victor-olimpio <victor.olimpio@gmail.com>
2019-09-17 08:44:28 -0300

    Atributos Pessoa e Usuario

--- app/src/main/java/br/ufrpe/wanderlustapp/pessoa/dominio/Pessoa.java
+++ app/src/main/java/br/ufrpe/wanderlustapp/pessoa/dominio/Pessoa.java
@@ -1,4 +1,24 @@
 package br.ufrpe.wanderlustapp.pessoa.dominio;
 
+import java.util.Date;
+
 public class Pessoa {
+    private String nome;
+    private Date nascimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
 }
