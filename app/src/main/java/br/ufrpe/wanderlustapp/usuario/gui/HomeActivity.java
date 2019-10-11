9dc07bfd24981afd99f85f4e242bc62f1ea1743a
jonathasfsilva <jonathas_ssilva@hotmail.com>
2019-10-11 11:55:58 -0300

    create gui close #5

---
+++ app/src/main/java/br/ufrpe/wanderlustapp/usuario/gui/HomeActivity.java
@@ -0,0 +1,16 @@
+package br.ufrpe.wanderlustapp.usuario.gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.ufrpe.wanderlustapp.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
