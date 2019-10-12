1e1aef328af8c854db41c0683021909432fc8119
Laiz Lira <laizgamalira@gmail.com>
2019-10-12 16:31:38 -0300

    PessoaServices

--- app/src/main/java/br/ufrpe/wanderlustapp/pessoa/negocio/PessoaServices.java
+++ app/src/main/java/br/ufrpe/wanderlustapp/pessoa/negocio/PessoaServices.java
@@ -1,4 +1,24 @@
 package br.ufrpe.wanderlustapp.pessoa.negocio;
 
+import android.database.Cursor;
+
+import br.ufrpe.wanderlustapp.infra.persistencia.DBHelper;
+import br.ufrpe.wanderlustapp.pessoa.dominio.Pessoa;
+import br.ufrpe.wanderlustapp.pessoa.persistencia.PessoaDAO;
+
+
 public class PessoaServices {
+    private PessoaDAO pessoaDAO = new PessoaDAO();
+
+    public long cadastrar(Pessoa pessoa) {
+        long idPessoa = pessoaDAO.cadastrar(pessoa);
+        return idPessoa;
+    }
+
+    public Pessoa getPessoa(long idPessoa) {
+        Pessoa pessoa;
+        pessoa = pessoaDAO.getPessoa(idPessoa);
+        return pessoa;
+    }
+
 }
