09eac2464ed5c53f73a06b8b0c63850e7de7ca8d
jonathasfsilva <jonathas_ssilva@hotmail.com>
2019-10-12 17:54:02 -0300

    home

--- app/src/main/java/br/ufrpe/wanderlustapp/usuario/gui/HomeActivity.java
+++ app/src/main/java/br/ufrpe/wanderlustapp/usuario/gui/HomeActivity.java
@@ -3,14 +3,26 @@ package br.ufrpe.wanderlustapp.usuario.gui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import br.ufrpe.wanderlustapp.R;
+import br.ufrpe.wanderlustapp.infra.Sessao;
+import br.ufrpe.wanderlustapp.usuario.dominio.Usuario;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TextView textoExibicao;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        usuario = Sessao.instance.getUsuario();
+        String nomeUsuario = usuario.getPessoa().getNome();
+        textoExibicao = findViewById(R.id.textView);
+        textoExibicao.setText("Bem vindo "+nomeUsuario);
+
     }
 }
