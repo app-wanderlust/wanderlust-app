86fa5975765fb67bae510b3f96d7b81d65125889
jonathasfsilva <jonathas_ssilva@hotmail.com>
2019-10-24 10:30:39 -0300

    metodos adapter

--- app/src/main/java/br/ufrpe/wanderlustapp/pratoTipico/gui/AdapterPratoTipico.java
+++ app/src/main/java/br/ufrpe/wanderlustapp/pratoTipico/gui/AdapterPratoTipico.java
@@ -8,10 +8,19 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 import br.ufrpe.wanderlustapp.R;
 import br.ufrpe.wanderlustapp.pratoTipico.dominio.PratoTipico;
 
 public class AdapterPratoTipico extends RecyclerView.Adapter<AdapterPratoTipico.MyViewHolder> {
+
+    private List<PratoTipico> pratoTipicoList;
+
+    public AdapterPratoTipico(List<PratoTipico> pratoTipicoList) {
+        this.pratoTipicoList = pratoTipicoList;
+    }
+
     @NonNull
     @Override
     public AdapterPratoTipico.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -22,8 +31,10 @@ public class AdapterPratoTipico extends RecyclerView.Adapter<AdapterPratoTipico.
 
     @Override
     public void onBindViewHolder(@NonNull AdapterPratoTipico.MyViewHolder holder, int position) {
-        PratoTipico pratoTipico =
-
+        PratoTipico pratoTipico = pratoTipicoList.get(position);
+        holder.prato.setText(pratoTipico.getNome());
+        holder.pais.setText(pratoTipico.getCidade().getPais().getNome());
+        holder.cidade.setText(pratoTipico.getCidade().getNome());
     }
 
     @Override
@@ -32,8 +43,14 @@ public class AdapterPratoTipico extends RecyclerView.Adapter<AdapterPratoTipico.
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView prato;
+        TextView pais;
+        TextView cidade;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            prato = itemView.findViewById(R.id.textNomePratoId);
+            pais = itemView.findViewById(R.id.textPaisId);
+            cidade = itemView.findViewById(R.id.textCidadeId);
         }
     }
 }
