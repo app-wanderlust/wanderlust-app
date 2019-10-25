a6e86a11c5e65816fabf8161ed3ffe6549d7b5df
jonathasfsilva <jonathas_ssilva@hotmail.com>
2019-10-25 13:09:06 -0300

    to do

--- app/src/main/java/br/ufrpe/wanderlustapp/cidade/negocio/CidadeServices.java
+++ app/src/main/java/br/ufrpe/wanderlustapp/cidade/negocio/CidadeServices.java
@@ -1,4 +1,5 @@
 package br.ufrpe.wanderlustapp.cidade.negocio;
 
 public class CidadeServices {
+    // TODO: 25/10/19 criar services para cidade
 }
--- app/src/main/java/br/ufrpe/wanderlustapp/pais/negocio/PaisServices.java
+++ app/src/main/java/br/ufrpe/wanderlustapp/pais/negocio/PaisServices.java
@@ -1,4 +1,5 @@
 package br.ufrpe.wanderlustapp.pais.negocio;
 
 public class PaisServices {
+    // TODO: 25/10/19 criar services para pais
 }
