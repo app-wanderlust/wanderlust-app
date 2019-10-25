ab3e874b8edd8bdb1619bbf40e45319e97d9d969
jonathasfsilva <jonathas_ssilva@hotmail.com>
2019-10-25 13:13:56 -0300

    cidadeServices

--- app/src/main/java/br/ufrpe/wanderlustapp/cidade/negocio/CidadeServices.java
+++ app/src/main/java/br/ufrpe/wanderlustapp/cidade/negocio/CidadeServices.java
@@ -1,4 +1,19 @@
 package br.ufrpe.wanderlustapp.cidade.negocio;
 
+import android.content.Context;
+
+import br.ufrpe.wanderlustapp.cidade.dominio.Cidade;
+import br.ufrpe.wanderlustapp.cidade.persistencia.CidadeDAO;
+
 public class CidadeServices {
+    private CidadeDAO cidadeDAO;
+
+    public CidadeServices(Context context) {
+        cidadeDAO = new CidadeDAO(context);
+    }
+
+    public void cadastrar(Cidade cidade){
+        long idCidade = cidadeDAO.cadastrar(cidade);
+        cidade.setId(idCidade);
+    }
 }
