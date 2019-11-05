8041bf1bd10fa50d54145a0c42b7fbae6800925e
maangelos <mariannaangelos.alves@gmail.com>
2019-11-05 12:26:17 -0300

    commit try bottom navegation by activities

--- app/src/main/java/br/ufrpe/wanderlustapp/infra/gui/BottomNavegationActivity.java
+++ app/src/main/java/br/ufrpe/wanderlustapp/infra/gui/BottomNavegationActivity.java
@@ -1,6 +1,7 @@
 package br.ufrpe.wanderlustapp.infra.gui;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -15,51 +16,51 @@ import br.ufrpe.wanderlustapp.R;
 import br.ufrpe.wanderlustapp.contato.gui.ContatoUtilFragment;
 import br.ufrpe.wanderlustapp.conversa.gui.ChatBotFragment;
 import br.ufrpe.wanderlustapp.pratoTipico.gui.CadastrarPratosFragment;
+import br.ufrpe.wanderlustapp.pratoTipico.gui.ListaPratosActivity;
 import br.ufrpe.wanderlustapp.usuario.gui.HomeActivity;
 import br.ufrpe.wanderlustapp.usuario.gui.HomeFragment;
+import br.ufrpe.wanderlustapp.usuario.gui.PerfilActivity;
 import br.ufrpe.wanderlustapp.usuario.gui.PerfilFragment;
 
 public class BottomNavegationActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigationView;
+    //private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navegation);
-        this.bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
-        mudarFragment(HomeFragment.newInstance());
+        //this.bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        //mudarFragment(HomeFragment.newInstance());
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()){
                     case R.id.nav_home:
-                        mudarFragment(HomeFragment.newInstance());
-                        return true;
+                        Intent intentHome = new Intent(BottomNavegationActivity.this,HomeActivity.class);
+                        startActivity(intentHome);
+                        break;
                     case R.id.nav_person:
-                        mudarFragment(PerfilFragment.newInstance());
-                        return true;
+                        Intent intentPerson = new Intent(BottomNavegationActivity.this, PerfilActivity.class);
+                        startActivity(intentPerson);
+                        break;
                     case R.id.nav_business:
-                        mudarFragment(ContatoUtilFragment.newInstance());
-                        return true;
+                        Intent intentContatos = new Intent(BottomNavegationActivity.this,ContatoUtilFragment.class);
+                        startActivity(intentContatos);
+                        break;
                     case R.id.nav_chat:
-                        mudarFragment(ChatBotFragment.newInstance());
-                        return true;
+                        break;
                     case R.id.nav_add:
-                        mudarFragment(CadastrarPratosFragment.newInstance());
-                        return true;
+                        Intent intentPratos = new Intent(BottomNavegationActivity.this, ListaPratosActivity.class);
+                        startActivity(intentPratos);
+                        break;
+
             }
                 return false;
             }
         });
     }
 
-    private void mudarFragment(Fragment fragment){
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
-        transaction.replace(R.id.frame,fragment);
-        transaction.commit();
-    }
 }
 
