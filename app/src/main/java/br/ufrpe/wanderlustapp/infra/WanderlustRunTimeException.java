dc85bd85885fe008bf381f118c8ee6cd2e85e96c
jonathasfsilva <jonathas_ssilva@hotmail.com>
2019-11-07 17:24:53 -0300

    clean infra

--- app/src/main/java/br/ufrpe/wanderlustapp/infra/WanderlustRunTimeException.java
+++ app/src/main/java/br/ufrpe/wanderlustapp/infra/WanderlustRunTimeException.java
@@ -1,9 +1,6 @@
 package br.ufrpe.wanderlustapp.infra;
 
 public class WanderlustRunTimeException extends RuntimeException{
-    public WanderlustRunTimeException(String msg) {
-        super(msg);
-    }
     public WanderlustRunTimeException(String msg, Throwable cause) {
         super(msg, cause);
     }
