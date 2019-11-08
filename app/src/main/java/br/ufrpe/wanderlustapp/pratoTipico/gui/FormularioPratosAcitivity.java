ca583e684611f88f9ea666dd8a72c35a89fc6a74
jonathasfsilva <jonathas_ssilva@hotmail.com>
2019-11-08 14:45:57 -0300

    cadastrar cidades close #20

--- app/src/main/java/br/ufrpe/wanderlustapp/pratoTipico/gui/FormularioPratosAcitivity.java
+++ app/src/main/java/br/ufrpe/wanderlustapp/pratoTipico/gui/FormularioPratosAcitivity.java
@@ -12,7 +12,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import br.ufrpe.wanderlustapp.R;
 import br.ufrpe.wanderlustapp.cidade.dominio.Cidade;
+import br.ufrpe.wanderlustapp.cidade.negocio.CidadeServices;
 import br.ufrpe.wanderlustapp.pais.dominio.Pais;
+import br.ufrpe.wanderlustapp.pais.negocio.PaisServices;
 import br.ufrpe.wanderlustapp.pratoTipico.dominio.PratoTipico;
 
 import static br.ufrpe.wanderlustapp.pratoTipico.gui.pratosActivityConstantes.CHAVE_PRATO;
@@ -24,6 +26,8 @@ public class FormularioPratosAcitivity extends AppCompatActivity {
     public static final String TITULO_APPBAR_INSERE = "Inserir prato";
     public static final String TITULO_APPBAR_ALTERA = "Alterar prato";
     private int posicaoRecebida;
+    CidadeServices cidadeServices = new CidadeServices(this);
+    PaisServices paisServices = new PaisServices(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -113,11 +117,13 @@ public class FormularioPratosAcitivity extends AppCompatActivity {
     }
 
     private Cidade createCidadePadrao() {
-        Cidade cidade = new Cidade();
-        cidade.setNome("Recife");
         Pais pais = new Pais();
         pais.setNome("Brasil");
+        paisServices.cadastrar(pais);
+        Cidade cidade = new Cidade();
+        cidade.setNome("Recife");
         cidade.setPais(pais);
+        cidadeServices.cadastrar(cidade);
         return cidade;
     }
 }
